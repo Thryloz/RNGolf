c3c6f79a19fb5eb02d26d6a13c69d40f74e911a7
Thryloz <86812547+Thryloz@users.noreply.github.com>
2024-02-02 16:39:34 -0800

    finished obstacle movement and shot, score, and %

--- src/Play.js
+++ src/Play.js
@@ -40,7 +40,14 @@ class Play extends Phaser.Scene {
         let wallA = this.physics.add.sprite(0, height/4, 'wall');
         wallA.setX(Phaser.Math.Between(0 + wallA.width/2, width - wallA.width/2))
         wallA.body.setImmovable(true)
-        
+        wallA.body.setCollideWorldBounds(true);
+        wallA.setVelocity(200, 0);
+        wallA.setBounce(1);
+
+
+
+
+
         let wallB = this.physics.add.sprite(0, height/2, 'wall');
         wallB.setX(Phaser.Math.Between(0 + wallB.width/2, width - wallB.width/2))
         wallB.body.setImmovable(true)
@@ -59,13 +66,15 @@ class Play extends Phaser.Scene {
             let shotDirectionX = pointer.x <= this.ball.x ? 1 : -1;
             this.ball.body.setVelocityX(Phaser.Math.Between(0, this.SHOT_VELOCITY_X) * shotDirectionX);
             this.ball.body.setVelocityY(Phaser.Math.Between(this.SHOT_VELOCITY_Y_MIN, this.SHOT_VELOCITY_Y_MAX) * shotDirection);
+            this.clickCounter += 1;
         })
         // cup/ball collision
         this.physics.add.collider(this.ball, this.cup, (ball, cup) => {
             this.ball.setX(width/2)
             this.ball.setY(height - height/10)
             this.ball.setVelocity(0,0)
-
+            this.shotCounter += 1;
+            
         })
 
         // ball/wall collision
@@ -74,10 +83,17 @@ class Play extends Phaser.Scene {
         // ball/one-way collision
         this.physics.add.collider(this.ball, this.oneWay)
 
+        this.shotCounter = 0;
+        this.clickCounter = 0;
+        this.shotCounterTxt = this.add.text(width-200, 10, `Shot Counter: ${this.shotCounter}`).setColor('black').setBackgroundColor('white');
+        this.scoreTxt = this.add.text(width-200, 40, `Score: ${this.shotCounter * 10}`).setColor('black').setBackgroundColor('white');
+        this.percentageTxt = this.add.text(width-200, 70, `Percentage: ${Math.round(this.shotCounter/this.clickCounter * 100)}%`).setColor('black').setBackgroundColor('white');
     }
 
     update() {
-
+        this.shotCounterTxt.text = `Shot Counter: ${this.shotCounter}`;
+        this.scoreTxt.text = `Score: ${this.shotCounter * 10}`;
+        this.percentageTxt.text = `Percentage: ${Math.round(this.shotCounter/this.clickCounter * 100)}%`;
     }
 }
 /*
@@ -85,6 +101,6 @@ CODE CHALLENGE
 Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
 [X] Add ball reset logic on successful shot
 [X] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
-[ ] Make one obstacle move left/right and bounce against screen edges
-[ ] Create and display shot counter, score, and successful shot percentage
+[X] Make one obstacle move left/right and bounce against screen edges
+[X] Create and display shot counter, score, and successful shot percentage
 */
